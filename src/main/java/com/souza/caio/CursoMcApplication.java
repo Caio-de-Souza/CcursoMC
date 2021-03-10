f2abbdd50a2961bb367c5df79e355f0a494d7262
Caio de Souza Lima <caio.lima@wises.com.br>
2021-03-09 22:56:01 -0300

    Adicao de ItemPedido e chave composta para tal

--- src/main/java/com/souza/caio/CursoMcApplication.java
+++ src/main/java/com/souza/caio/CursoMcApplication.java
@@ -13,6 +13,7 @@ import com.souza.caio.domain.Cidade;
 import com.souza.caio.domain.Cliente;
 import com.souza.caio.domain.Endereco;
 import com.souza.caio.domain.Estado;
+import com.souza.caio.domain.ItemPedido;
 import com.souza.caio.domain.Pagamento;
 import com.souza.caio.domain.PagamentoComBoleto;
 import com.souza.caio.domain.PagamentoComCartao;
@@ -144,6 +145,20 @@ public class CursoMcApplication implements CommandLineRunner{
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		
+		
 	}
 
 }
