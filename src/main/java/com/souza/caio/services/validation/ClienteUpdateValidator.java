88ef08bfb0314c3b6a70294cce14accba1e66656
Caio de Souza Lima <caio.lima@wises.com.br>
2021-03-15 20:58:44 -0300

    Implementacao de logica para verificar duplicacao de email na atualizacao de dados de Cliente

---
+++ src/main/java/com/souza/caio/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,60 @@
+package com.souza.caio.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.souza.caio.domain.Cliente;
+import com.souza.caio.domain.enums.TipoCliente;
+import com.souza.caio.dto.ClienteDTO;
+import com.souza.caio.repositories.ClienteRepository;
+import com.souza.caio.resources.exceptions.FieldMessage;
+import com.souza.caio.services.validation.utils.BR;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = 
+				(Map<String, String>) request.getAttribute(
+				HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE //TUTORIAL: Retorna o mapping de variaveis de URI que estão na requisição
+		);
+		
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+
+		//TUTORIAL: inclua os testes aqui, inserindo erros na lista
+		Cliente clienteExistente = clienteRepository.findByEmail(objDto.getEmail());
+		
+		if(clienteExistente != null && !(clienteExistente.getId().equals(uriId))) {
+			list.add(new FieldMessage("email", "E-mail já existente"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
